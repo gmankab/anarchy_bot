9f36eafc3028ee08130e47795199c96c3aa95ea3
Gmanka <gmankab@gmail.com>
2023-07-12 16:14:07 +0400

    init

--- anarchy_bot/config.py
+++ anarchy_bot/config.py
@@ -23,7 +23,7 @@ except Exception:
 
 
 app_name = 'anarchy_bot'
-app_version = '23.0.1'
+app_version = '23.0.2'
 c = rich.console.Console()
 c.log(f'imported [deep_sky_blue1]{__file__}')
 app_path = Path(
